9140c45f4d9f91bfe966f64aa3d759e52ae63f46
jamesli <li.jin.feng.2008@163.com>
2016-02-25 17:35:43 +0800

    change application.properties

--- src/main/java/com/smart/shop/Application.java
+++ src/main/java/com/smart/shop/Application.java
@@ -1,20 +1,59 @@
 package com.smart.shop;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+/**
+ * 微服务应用入口
+ * @author lijinfeng
+ * @time 2016-02-25 16:32
+ * @remark 应用访问入口
+ * 1. @RestController 
+ * 2. @Controller 模板下使用
+ */
+@Controller
 @EnableAutoConfiguration
 public class Application {
-
+	/**
+	 * 首页
+	 * @return
+	 */
     @RequestMapping("/")
-    String home() {
+    String index() {
+        return "index";
+    }
+    
+    /**
+     * 登录页面
+     * @return
+     */
+    @RequestMapping("/login")
+    public String login() {
         return "Hello World!";
     }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+    
+    /**
+     * 退出页面
+     * @return
+     */
+    @RequestMapping("/logout")
+    public String logout() {
+        return "Hello World!";
+    }
+    
+    /**
+     * 主函数
+     * @param args
+     * @throws Exception
+     */
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+        //SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setShowBanner(false);
+        app.run(args);
     }
 
 }

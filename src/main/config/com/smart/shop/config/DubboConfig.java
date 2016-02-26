fe25f9e1f07e2ce8df8ede28779625060b9de70a
jamesli <li.jin.feng.2008@163.com>
2016-02-26 13:57:14 +0800

    change smart-dream-shop

--- src/main/config/com/smart/shop/config/DubboConfig.java
+++
@@ -1,9 +0,0 @@
-package com.smart.shop.config;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:META-INF/spring/service-context.xml")
-public class DubboConfig {
-
-}
--- src/main/config/com/smart/shop/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.smart.shop.config;
-
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-//@SpringBootApplication
-//@ComponentScan(basePackages = "boot")
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-}
